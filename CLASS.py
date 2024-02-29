91be6b543797e42073d2bff897f944c94f5ef029
mdAMAN422 <bappi.port@gmail.com>
2024-02-29 20:05:09 +0530

    first commit

--- CLASS.py
+++ CLASS.py
@@ -4,7 +4,7 @@ class clo:
     def otp(self):
         list = [14,25,36,96,85,74,12,32,456,]
         print(list[5])
-        var2 = "my name is kamarul"
+        var2 = "HELLO EVERYONE"
         var = 1
         while var <= 10:
             var += 1
